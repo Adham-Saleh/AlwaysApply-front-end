969dd23f833a171b15c0ae81dec6bcc0fc1f725a
Adham-Saleh <adhamsaleh055@gmail.com>
2024-12-18 20:29:30 +0200

    fix deployment bugs: runTime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,7 +34,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      API_BASE_URL: process.env.API_BASE_URL || "http://localhost:8000",
+      API_BASE_URL: process.env.API_BASE_URL || "https://adhamsaleh.pythonanywhere.com/",
     },
   },
   app: {
